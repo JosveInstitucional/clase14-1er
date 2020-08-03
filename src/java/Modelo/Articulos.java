3b5e9d9b1b0d903d3909fa6892f5a2f81318587a
Hebert Flores <2017110474@untels.edu.pe>
2020-08-03 12:28:51 -0500

    agregar archivos

--- src/java/Modelo/Articulos.java
+++ src/java/Modelo/Articulos.java
@@ -10,5 +10,80 @@ package Modelo;
  * @author USER
  */
 public class Articulos {
-    
+    private int idarticulo;
+    private String nombre;
+    private double precio;
+    private int stock;
+    private String marca;       
+
+    /**
+     * @return the idarticulo
+     */
+    public int getIdarticulo() {
+        return idarticulo;
+    }
+
+    /**
+     * @param idarticulo the idarticulo to set
+     */
+    public void setIdarticulo(int idarticulo) {
+        this.idarticulo = idarticulo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the stock
+     */
+    public int getStock() {
+        return stock;
+    }
+
+    /**
+     * @param stock the stock to set
+     */
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
+
